e54e08202ddbbaa4cbddc6b78ef7ea6c677be1bb
artjac19 <67008854+artjac19@users.noreply.github.com>
2023-12-21 22:40:35 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+ const myHeading = document.querySelector('h1')
+myHeading.textContent = 'Wordle'
+
+//Subtitle... same thing different field
+const mySubText = document.querySelector('b')
+mySubText.textContent = 'not by the New York Times'
