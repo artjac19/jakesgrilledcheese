832ea6207eb3735f56a7f718edf52d21bd1e7ad7
artjac19 <67008854+artjac19@users.noreply.github.com>
2023-12-21 23:01:25 -0600

    a

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
- const myHeading = document.querySelector('h1')
+ const myHeading = document.querySelector('body')
 myHeading.textContent = 'Wordle'
 
 //Subtitle... same thing different field
-const mySubText = document.querySelector('b')
+const mySubText = document.querySelector('body')
 mySubText.textContent = 'not by the New York Times'
