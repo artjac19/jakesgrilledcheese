9858118d3cf1209dc06530f28713e9e3a2b4c56e
artjac19 <67008854+artjac19@users.noreply.github.com>
2023-12-21 23:29:53 -0600

    k

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
- const myHeading = document.querySelector('body')
-myHeading.textContent = 'Wordle'
+document.addEventListener('DOMContentLoaded', function () {
+    // Function to create and style a heading
+    function createAndStyleHeading() {
+        const headingContainer = document.createElement('div');
+        headingContainer.classList.add('heading-container'); // Add a custom class for styling
 
-//Subtitle... same thing different field
-const mySubText = document.querySelector('body')
-mySubText.textContent = 'not by the New York Times'
+        const myHeading = document.createElement('h1');
+        myHeading.textContent = 'Welcome';
+        myHeading.classList.add('fancy-text', 'larger-text', 'centered-text'); // Add custom classes for styling
+
+        headingContainer.appendChild(myHeading);
+        document.body.appendChild(headingContainer);
+    }
+
+    // Function to create and style a subtitle
+    function createAndStyleSubtitle() {
+        const subtitleContainer = document.createElement('div');
+        subtitleContainer.classList.add('subtitle-container'); // Add a custom class for styling
+
+        const mySubText = document.createElement('h2');
+        mySubText.textContent = 'Jake\'s Grilled Cheese';
+        mySubText.classList.add('fancy-text', 'larger-text', 'centered-text'); // Add custom classes for styling
+
+        subtitleContainer.appendChild(mySubText);
+        document.body.appendChild(subtitleContainer);
+    }
+
+    // Call the functions to create and style the heading and subtitle
+    createAndStyleHeading();
+    createAndStyleSubtitle();
+});
+
