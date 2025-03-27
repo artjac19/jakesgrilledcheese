d38f439c5faec5dab622908344b7f15c558e9920
artjac19 <67008854+artjac19@users.noreply.github.com>
2025-03-27 14:14:31 -0500

    Update main.js

--- main.js
+++ main.js
@@ -12,9 +12,9 @@ document.addEventListener('DOMContentLoaded', function () { });
 function selectOption(option, event) {
     event.preventDefault();
     var dropdownButton;
-    if (option == "Select Month" || option == "  December ") { dropdownButton = document.getElementById("dropdownButton1"); }
-    if (option == " Select Day " || option == "   25th   ") { dropdownButton = document.getElementById("dropdownButton2"); }
-    if (option == "Select Time " || option == "   7:00 pm  ") { dropdownButton = document.getElementById("dropdownButton3"); }
+    if (option == "Select Month" || option == "  March ") { dropdownButton = document.getElementById("dropdownButton1"); }
+    if (option == " Select Day " || option == "   27th   ") { dropdownButton = document.getElementById("dropdownButton2"); }
+    if (option == "Select Time " || option == "   6:30 pm  ") { dropdownButton = document.getElementById("dropdownButton3"); }
     dropdownButton.innerHTML = option;
     toggleDropdown(); // Close the dropdown after selecting an option
 }
@@ -44,7 +44,7 @@ function toggleDropdown3() {
 }
 
 function reserveClick() {
-    if (document.getElementById("dropdownButton1").innerHTML == "  December " && document.getElementById("dropdownButton2").innerHTML == "   25th   " && document.getElementById("dropdownButton3").innerHTML == "   7:00 pm  ") {
+    if (document.getElementById("dropdownButton1").innerHTML == "  March " && document.getElementById("dropdownButton2").innerHTML == "   27th   " && document.getElementById("dropdownButton3").innerHTML == "   6:30 pm  ") {
         document.getElementById("btn").innerHTML = "Reserved!";
         document.getElementById("btn").classList.remove('reserveFailed');
         document.getElementById("btn").classList.add('reserved');
